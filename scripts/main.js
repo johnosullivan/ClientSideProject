e2c5438aaf193bb35ee7b3c0206b640fc0b95383
John O'Sullivan <josullivan1@luc.edu>
2017-02-14 11:12:06 -0600

    Created simple page structure

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+/* openMenu for openning the menu */
+function openMenu() {
+   document.getElementById("menu").style.display = "block";
+}
+/* closeMenu for closing the menu */
+function closeMenu() {
+   document.getElementById("menu").style.display = "none";
+}
