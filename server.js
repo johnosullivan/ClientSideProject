44d0634cc4b8543f4c2b98b3baf1066d7e39fffd
Kathryn Eaton <keaton2@luc.edu>
2017-04-03 17:06:07 -0500

    added server.js file, edited HEADs and merge errors

---
+++ server.js
@@ -0,0 +1,45 @@
+/**
+ * Created by keetz on 4/3/17.
+ */
+/* a simple Express server for Node.js
+ comp 424 - appTest
+ */
+
+var express = require("express"),
+    http = require("http"),
+    bodyParser = require("body-parser"),
+    jsonApp = express();
+
+var trips = {
+    "trips": [{
+        "flightID": "ABC123",
+        "startLocation": "6711 N Lakewood Avenue"
+    }]
+};
+
+//set as static file server...
+jsonApp.use(express.static(__dirname + "/app"));
+
+//parse jQuery JSON to useful JS object
+jsonApp.use(bodyParser.urlencoded({ extended: false }));
+
+//create http server
+//TODO: make a local server
+///http.createServer(jsonApp).listen(3030);
+
+//json get route
+//TODO: Make our root json file
+jsonApp.get("/trips.json", function(req, res) {
+    res.json(trips);
+});
+
+jsonApp.post("/trips", function(req, res) {
+    //store new object in req.body
+    var newNote = req.body;
+    //push new note to JSON
+    trips["trips"].push(newTrip);
+    //return simple JSON object
+    res.json({
+        "message": "post complete to server"
+    });
+});
