9d063eb53eeab9531562817743de2224b5b70f25
Kathryn Eaton <keaton2@luc.edu>
2017-04-17 18:31:31 -0500

    added json test data but moved stuff and needs work

--- server/server.js
+++ server/server.js
@@ -7,8 +7,13 @@
 
 var express = require("express"),
     http = require("http"),
-    bodyParser = require("body-parser"),
-    jsonApp = express();
+    //bodyParser = require("body-parser"),
+    testApp;
+
+testApp = express();
+http.createServer(testApp).listen();
+
+
 
 var trips = {
     "trips": [{
@@ -19,28 +24,28 @@ var trips = {
 };
 
 //set as static file server...
-jsonApp.use(express.static(__dirname + "/app"));
+//jsonApp.use(express.static(__dirname + "/app"));
 
 //parse jQuery JSON to useful JS object
-jsonApp.use(bodyParser.urlencoded({ extended: false }));
+//jsonApp.use(bodyParser.urlencoded({ extended: false }));
 
 //create http server
 //TODO: make a local server
 ///http.createServer(jsonApp).listen(3030);
 
 //json get route
-//TODO: Make our root json file
-jsonApp.get("/trips.json", function(req, res) {
-    res.json(trips);
-});
-
-jsonApp.post("/trips", function(req, res) {
-    //store new object in req.body
-    var newNote = req.body;
-    //push new note to JSON
-    trips["trips"].push(newTrip);
-    //return simple JSON object
-    res.json({
-        "message": "post complete to server"
-    });
-});
+// //TODO: Make our root json file
+// jsonApp.get("/trips.json", function(req, res) {
+//     res.json(trips);
+// });
+//
+// jsonApp.post("/trips", function(req, res) {
+//     //store new object in req.body
+//     var newNote = req.body;
+//     //push new note to JSON
+//     trips["trips"].push(newTrip);
+//     //return simple JSON object
+//     res.json({
+//         "message": "post complete to server"
+//     });
+// });
