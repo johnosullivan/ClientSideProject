704e4a41f26f84e91b7242673b61226397165529
Kathryn Eaton <keaton2@luc.edu>
2017-04-22 20:18:36 -0500

    edited main, background styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,31 @@
-$(document).ready(function(){
-  "use strict";
-  // Grab the data from the input fields and build the url to set as location
-  $("#go").click(function() {
-    var flight = $('#flight').val();
-    var location = $('#location').val();
-    window.location = 'map.html?flight=' + flight + '&location=' + location;
-    $("#flight").val("");
-    $("#location").val("");
-  });
+$(document).ready(function() {
+    "use strict";
+    //random background generator
+    var images = ['images/chicago.jpg',
+        'images/newyork.jpg',
+        'images/sanfran.jpg',
+        'images/city1.jpg',
+        'images/city2.jpg',
+        'images/city3.jpg',
+        'images/city4.jpg',
+        'images/city5.jpg',
+        'images/city6.jpg'];
+
+        var randomNumber = Math.floor(Math.random() * images.length);
+        var bgImg = 'url(' + images[randomNumber] + ')';
+
+        $('body').css({'background': bgImg, 'background-size': 'cover'});
+        ;
+
+    // Grab the data from the input fields and build the url to set as location
+    $("#go").click(function () {
+        var flight = $('#flight').val();
+        var location = $('#location').val();
+        window.location = 'map.html?flight=' + flight + '&location=' + location;
+        $("#flight").val("");
+        $("#location").val("");
 
 
-  var images = ['images/chicago.jpg',
-               'images/newyork.jpg',
-               'images/sanfran.jpg',
-               'images/city1.jpg',
-               'images/city2.jpg',
-               'images/city3.jpg',
-               'images/city4.jpg',
-               'images/city5.jpg',
-               'images/city6.jpg'];
+    })
 
-  var randomNumber = Math.floor(Math.random() * images.length);
-  var bgImg = 'url(' + images[randomNumber] + ') no-repeat center center fixed;';
-
-  $('body').css({'background': bgImg, 'background-size': 'cover', '-webkit-background-size': 'cover', '-moz-background-size': 'cover', '-o-background-size': 'cover'});
 });
